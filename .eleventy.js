f88bf6afca8e567003b8bb32776a285977d770d8
Saeed Mirshekari <saeed.mirshekari@gmail.com>
2022-03-12 14:21:08 -0600

    blogify

---
+++ .eleventy.js
@@ -0,0 +1,24 @@
+const { DateTime} = require("luxon");
+
+module.exports = function(eleventyConfig) {
+
+    eleventyConfig.addPassthroughCopy('/_src/assets');
+    eleventyConfig.addPassthroughCopy('/_publica/assets');
+    eleventyConfig.addFilter("postDate", (dateObj) => {
+        return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
+    });
+
+    eleventyConfig.addCollection("feed", collection => {
+        return collection
+          .getFilteredByTag("post")
+          .reverse()
+          .slice(0, 5);
+      });
+
+    return {
+        dir: {
+            input: "_src",
+            output: "_public"
+        }
+    };
+}
