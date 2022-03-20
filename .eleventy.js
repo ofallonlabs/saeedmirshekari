47a084efb0a4898f33e707eb3b0958ead9bb0a19
Saeed Mirshekari <saeed.mirshekari@gmail.com>
2022-03-19 19:10:23 -0500

    passforward

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy('./_src/assets');
     eleventyConfig.addPassthroughCopy('./_src/admin');
     eleventyConfig.addPassthroughCopy('./_src/blog');
+    eleventyConfig.addPassthroughCopy('./_src/winners.txt');
+    eleventyConfig.addPassthroughCopy('./_src/terms_of_service.txt');
+    eleventyConfig.addPassthroughCopy('./_src/terms_of_service.html');
+    eleventyConfig.addPassthroughCopy('./_src/privacy_policy.html');
+
 
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
