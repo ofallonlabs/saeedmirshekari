866e873f5e8abda1c8e1bdd48e51650bc9d2270b
Saeed Mirshekari <info@saeedmirshekari.com>
2023-12-15 11:01:37 -0600

    sas

--- .eleventy.js
+++ .eleventy.js
@@ -5,7 +5,7 @@ module.exports = function(eleventyConfig) {
     // eleventyConfig.addPassthroughCopy('./_src/assets');
     eleventyConfig.addPassthroughCopy('./_src/admin');
     eleventyConfig.addPassthroughCopy('./_src/blog');
-    eleventyConfig.addPassthroughCopy('./_src/winners.txt');
+    // eleventyConfig.addPassthroughCopy('./_src/winners.txt');
     // eleventyConfig.addPassthroughCopy('./_src/terms_of_service.txt');
     // eleventyConfig.addPassthroughCopy('./_src/terms_of_service.html');
     // eleventyConfig.addPassthroughCopy('./_src/privacy_policy.html');
