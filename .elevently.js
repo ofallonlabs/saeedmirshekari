85ee70b80c64990c6a435271c072a9f85c2ea238
Saeed Mirshekari <info@saeedmirshekari.com>
2024-05-25 16:16:48 -0500

    sas

--- .elevently.js
+++ .elevently.js
@@ -2,14 +2,11 @@ const {DateTime} = require("luxon");
 
 module.exports = function(eleventyConfig) {
 
-    eleventyConfig.addPassthroughCopy('./_src/assets');
     eleventyConfig.addPassthroughCopy('./_src/admin');
     eleventyConfig.addPassthroughCopy('./_src/blog');
     // eleventyConfig.addPassthroughCopy('./_src/terms_of_service.txt');
     // eleventyConfig.addPassthroughCopy('./_src/terms_of_service.html');
     // eleventyConfig.addPassthroughCopy('./_src/privacy_policy.html');
-
-
 
     eleventyConfig.addFilter("postDate", (dateObj) => {
         return DateTime.fromJSDate(dateObj).toLocaleString(DateTime.DATE_MED)
