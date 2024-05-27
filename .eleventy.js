69551c093de3b595c21eea5bc9f876f13ca88ebd
Saeed Mirshekari <info@saeedmirshekari.com>
2024-05-27 12:51:46 -0500

    ok

---
+++ .eleventy.js
@@ -0,0 +1,37 @@
+const { DateTime } = require("luxon");
+
+
+module.exports = function(eleventyConfig) {
+
+    // Adding the custom filter
+    eleventyConfig.addFilter("dateFull", (dateObj) => {
+        return DateTime.fromJSDate(dateObj, { zone: 'utc' }).toLocaleString(DateTime.DATE_FULL);
+    });
+
+
+    eleventyConfig.addLayoutAlias('default', 'base.njk');
+
+
+    eleventyConfig.addPassthroughCopy('./_src/admin');
+    // eleventyConfig.addPassthroughCopy('./_src/blog');
+    // eleventyConfig.addPassthroughCopy('./_src/terms_of_service.txt');
+    eleventyConfig.addPassthroughCopy('./_src/terms_of_service.html');
+    eleventyConfig.addPassthroughCopy('./_src/privacy_policy.html');
+
+
+    eleventyConfig.addCollection("feed", function (collectionApi) {
+        return collectionApi.getAllSorted().reverse().slice(0, 5);
+      });
+
+    eleventyConfig.addCollection("allposts", function(collection) {
+        return collection.getFilteredByTags("post", "mentoring", "career", "projects");
+    });
+
+    return {
+        dir: {
+            input: "_src",
+            output: "_public"
+        }
+    };
+
+}
